7844dc8f8fef1c0974e4a57f1b01c4e04d2cca20
Amel Ouferhat <ouferhatamel@gmail.com>
2022-07-24 13:05:14 +0100

    showing lists on the faqs paragraphs and adding the faqs link tothe footer

--- js/faqs.js
+++ js/faqs.js
@@ -2,14 +2,24 @@ let dropdown = document.querySelectorAll('.questionTitle img');
 
 dropdown.forEach(drop =>{
     drop.addEventListener('click', e =>{
+        
         const parentElm = e.currentTarget.parentElement.parentElement;
         const parags = parentElm.querySelectorAll('p');
 
+        // Rotate the dropdown
         drop.classList.toggle("opened");
 
+        // Show question paragraphs
         parags.forEach(parag =>{
             parag.classList.toggle('shown');
-        })
-        
+        });
+
+        // Show lists inside paragraphs
+        if(parentElm.querySelector("ol") != null){
+            parentElm.querySelector("ol").classList.toggle('opened');
+        }
+        if(parentElm.querySelector("ul") != null){
+            parentElm.querySelector("ul").classList.toggle('opened');
+        }
     })
 })
