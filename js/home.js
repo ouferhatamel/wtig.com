b55982086ff1e7d433371d929452036d519348f8
Amel Ouferhat <ouferhatamel@gmail.com>
2022-07-30 01:05:03 +0100

    Translating the navbar, the footer and the header of the home page onto english and spanish

--- js/home.js
+++ js/home.js
@@ -3,14 +3,42 @@ import {
     doc,
     getDoc,
 } from "./modules/firebaseSdk.js";
-/* const langBtn = document.querySelector(".mnFtr__lang h4");
+
+import { langObj } from "./modules/langages.js";
+
+const langDropdown = document.querySelector(".mnFtr__lang h4");
 const mangList = document.querySelector('.mnFtr__langList');
 
-langBtn.addEventListener('click', () =>{
-    mangList.classList.toggle('mnFtr__langList--visible')
-}) */
+// Show langage list
+langDropdown.addEventListener('click', () =>{
+    mangList.classList.toggle('mnFtr__langList--visible');
+    checkLangage();
+}) 
 
-//TODO :: Check if card exists
+// Check the selected langage
+checkLangage();
+
+// Translate to english
+const eng = document.getElementById("mnFtr_eng");
+eng.addEventListener('click', e => {
+    // Change the local storage value
+    let lang = "en";
+    localStorage.setItem('selectedLang', lang);
+
+    // Translate
+    translate(lang);
+});
+
+// Translate to spanish
+const esp = document.getElementById("mnFtr_esp");
+esp.addEventListener('click', e => {
+    // Change the local storage value
+    let lang = "esp";
+    localStorage.setItem('selectedLang', lang);
+
+    // Translate
+    translate(lang);
+});
 
 // Check card authenticity
 const checkBtn = document.getElementById('checkAuth__checkBtn');
@@ -28,173 +56,7 @@ checkBtn.addEventListener('click', (e) => {
     
 });
 
-
-///////////////////////
-//Functions///////////
-//////////////////////
-//Open a new page to the authenticity certificate
-/* function openNewPage(){
-    //Get the input value (The code)
-    const code = document.getElementById('checkAuth__input').value;
-    let link = window.open("check-authenticity.html");
-    link.document.writeln(`
-    <!DOCTYPE html>
-    <html lang="es">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="short cut icon" href="images/icons/wytgrd-abbr-logo-icon.svg">
-        <link rel="stylesheet" href="scss/pages/check-auth.css">
-        <title>Comprobar la autenticidad</title>
-    </head>
-        <body>
-            <!--NAVIGATION BAR-->
-            <nav class="navBar">
-                <a href="index.html"><img src="images/others/Willim&Turnergrd-NavBar-logo.svg" alt="Willim&Turnergrd-NavBar-logo"></a>
-                <div class="navBar__links">
-                    <div class="navBar__detailsLinks">
-                        <a href="services.html#tarifs">TARIFAS</a>
-                        <a href="services.html">SERVICIOS</a>
-                    </div>
-                    <div class="navBar__detailsLinks">
-                        <a href="submit-card.html" id='navBar__certify'>¡CERTIFICAR!</a>
-                        <a href="register.html" id="navBar__login">LOGIN</a>
-                        <a href="myAccount.html" id="navBar__myAccount">MI CUENTA</a>
-                        <a href="mailto:info@williamturner-grading.com">CONTACTO</a>
-                    </div>
-                </div>
-            </nav>
-            <!-- MENU BURGER BTN -->
-            <div class="burger">
-                <div class="burger__bar"></div>
-            </div>
-            <!-- MAIN CONTENT -->
-            <main id="cerficate">
-                <!-- Certificate header -->
-                <div class="certificate__header">
-                    <img src="images/logos/wytgrd-grading-company-logo.svg" alt="wtig-grading-company-logo">
-                    <h1>Certificado de autenticidad</h1>
-                </div>
-                <!-- Certificate label -->
-                <div class="certificate__label">
-                    <div class="certificate__label__img"></div>
-                    <div class="certificate__label__info">
-                        <ul>
-                            <li>
-                                <div>Código de verificación</div>
-                                <div id="certificate__code">7840</div>
-                            </li>
-                            <li>
-                                <div>Nota</div>
-                                <div id="certificate__note">
-                                    8<br>near mint-mint
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <!-- Certificate details -->
-                <div class="certificate__details">
-                    <ul>
-                        <li>
-                            <span>Juego de cartas</span>
-                            <span>Pokemon</span>
-                        </li>
-                        <li>
-                            <span>Nombre del espécimen</span>
-                            <span>Charizard</span>
-                        </li>
-                        <li>
-                            <span>Set</span>
-                            <span>Basic</span>
-                        </li>
-                        <li>
-                            <span>Idioma del certificado</span>
-                            <span>Inglés</span>
-                        </li>
-                        <li>
-                            <span>Idioma de la tarjeta</span>
-                            <span>Japonés</span>
-                        </li>
-                    </ul>
-                </div>
-                <!-- Certificate footer -->
-                <div class="certificate__footer">
-                    <div>
-                        <div class="title">Fecha</div>
-                        <div id="certificate__date">06/06/2022</div>
-                    </div>
-                    <div>
-                        <div class="title">Presidente</div>
-                        <img src="images/others/wtig-president-signature-test.png" alt="wtig-president-signature" id="certificate__signature"></img>
-                    </div>
-                </div>
-            </main>
-            <!-- CHECK ANOTHER CARD -->
-            <div class="check_authenticity">
-                <a href="index.html#checkAuth">Comprobar otra tarjeta</a>
-                <h3>Gracias por utilizar nuestro sistema de certificación</h3>
-            </div>
-            
-            <!-- SCRIPTS -->
-            <script type="module" src="js/modules/updateNav-ui.js"></script>
-            <!--FOOTER-->
-            <footer class="mnFtr">
-                <div class="mnFtr__container">
-                    <!--Col01-->
-                    <div class="mnFtr__links mnFtr__logo">
-                        <img src="images/logos/wytgrd-grading-company-logo.svg" alt="wytgrd-grading-company-logo">
-                    </div>
-                    <!--Col02-->
-                    <div class="mnFtr__links">
-                        <h4>Ayuda</h4>
-                        <ul>
-                            <li><a href="services.html">Sobre nostros</a></li>
-                            <li><a href="services.html#tarifs">Tarifas</a></li>
-                            <li><a href="#">Faq</a></li>
-                            <li><a href="mailto:info@williamturner-grading.com">Contactar</a></li>
-                        </ul>
-                    </div>
-                    <!--Col03-->
-                    <div class="mnFtr__links">
-                        <h4>Mi cuenta</h4>
-                        <ul>
-                            <li><a href="myAccount.html">Mi cuenta</a></li>
-                            <li><a href="myAccount.html">Mis pedidos</a></li>
-                        </ul>
-                    </div>
-                    <!--Col04-->
-                    <div class="mnFtr__links">
-                        <h4>Servicios</h4>
-                        <ul>
-                            <li><a href="#">Nuestras garantias</a></li>
-                            <li><a href="submit-card.html">Certificar una carta</a></li>
-                            <li><a href="index.html#checkAuth">Verificar la autenticidad</a></li>
-                        </ul>
-                    </div>
-                    <!--Col05-->
-                    <div class="mnFtr__links">
-                        <div class="mnFtr__lang">
-                            <h4>Español</h4>
-                            <img src="images/icons/wytgrd-dropdown-icon.svg" alt="wytgrd-dropdown-icon">
-                        </div>
-                        
-                        <ul class="mnFtr__langList">
-                            <li><a href="#">English</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="mntFtr__copyright">
-                    <p>©2022 WYTGRD - Todos los derechos reservados | Aviso legal | Condiciones generales de venta | Politica de privacidad
-                    </p>
-                </div>
-            </footer>
-        </body>
-    </html>
-    `);
-
-} */
+// Functions
 
 // Check if card exists
 async function cardExists(code){
@@ -247,4 +109,66 @@ function showNoCardMessage(msg){
         inputText.textContent = "¡COMPRUEBA LA AUTENTICIDAD DE TU CARTA!";
         inputText.style.color = "#202223";
     }, 3000);
+}
+// Check the selected langage
+function checkLangage(){
+    const lang = localStorage.getItem('selectedLang');
+    if (lang == null)
+        return;
+    else
+        translate(lang);
+}
+// Translate the page
+function translate(lang){
+    // Translate navigation bar and footer
+    translateNavBarAndFooter(lang);
+
+    // Translate the header
+    const headlines = document.querySelectorAll('.heroDescription');
+    headlines[0].querySelector("h1").innerHTML =langObj[lang]["home"]["h1S1"];
+    headlines[1].querySelector("h1").innerHTML =langObj[lang]["home"]["h1S2"];
+    headlines[2].querySelector("h1").innerHTML =langObj[lang]["home"]["h1S3"];
+    headlines[3].querySelector("h1").innerHTML =langObj[lang]["home"]["h1S1"];
+    headlines[0].querySelector("p").innerHTML =langObj[lang]["home"]["p01"];
+    headlines[1].querySelector("p").innerHTML =langObj[lang]["home"]["p01"];
+    headlines[2].querySelector("p").innerHTML =langObj[lang]["home"]["p01"];
+    headlines[3].querySelector("p").innerHTML =langObj[lang]["home"]["p01"];
+    document.querySelector('.hero__certifyBtn').textContent = langObj[lang]["home"]["cta"];
+
+    // Translate the advantage section
+
+
+    
+    
+}
+function translateNavBarAndFooter(lang){
+    // Translate the navBar
+    const navLinks= document.querySelectorAll('.navBar__detailsLinks');
+    const n0= navLinks[0].querySelectorAll("a");
+    const n1= navLinks[1].querySelectorAll("a");
+
+    n0[0].textContent = langObj[lang]["navBar"]["tarif"];
+    n0[1].textContent = langObj[lang]["navBar"]["services"];
+    n1[0].textContent = langObj[lang]["navBar"]["certify"];
+    n1[2].textContent = langObj[lang]["navBar"]["myAccount"];
+    n1[3].textContent = langObj[lang]["navBar"]["contact"];
+    
+    // Translate the footer
+    const footerLinks = document.querySelectorAll(".mnFtr__links");
+    const c1 = footerLinks[1].querySelector("ul").querySelectorAll("li");
+    const c2 = footerLinks[2].querySelector("ul").querySelectorAll("li");
+    const c3 = footerLinks[3].querySelector("ul").querySelectorAll("li");
+    footerLinks[1].querySelector("h4").textContent = langObj[lang]["footer"]["help"];
+    footerLinks[2].querySelector("h4").textContent = langObj[lang]["footer"]["myAccount"];
+    footerLinks[3].querySelector("h4").textContent = langObj[lang]["footer"]["services"];
+    footerLinks[4].querySelector("h4").textContent = langObj[lang]["footer"]["langage"];
+    
+    c1[0].textContent = langObj[lang]["footer"]["aboutUs"];
+    c1[1].textContent = langObj[lang]["footer"]["tarif"];
+    c1[3].textContent = langObj[lang]["footer"]["contact"];
+    c2[0].textContent = langObj[lang]["footer"]["myAccount"];
+    c2[1].textContent = langObj[lang]["footer"]["myOrders"];
+    c3[0].textContent = langObj[lang]["footer"]["warranties"];
+    c3[1].textContent = langObj[lang]["footer"]["certifyCard"];
+    c3[2].textContent = langObj[lang]["footer"]["checkAuth"];
 }
---
+++ js/modules/langages.js
@@ -0,0 +1,84 @@
+const langObj = {
+    en: {
+        navBar: {
+            tarif: "PRICING",
+            services: "SERVICES",
+            certify: "CERTIFY",
+            myAccount: "MY ACCOUTNT",
+            contact: "CONTACT"
+        },
+        footer: {
+            help: "Help",
+            myAccount: "My account",
+            services: "Services",
+            langage: "English",
+            aboutUs: "About us",
+            tarif: "Pricing",
+            faqs: "FAQs",
+            contact: "Contact",
+            myOrders: "My orders",
+            warranties: "Our warranties",
+            certifyCard : "Certify a card",
+            checkAuth: "Verify authenticity",
+            rightsReserved: "All rights reserved",
+            legalNotice: "Legal Notice",
+            saleConditions: "Terms and conditions of sale",
+            privacyPolicy: "Privacy Policy"
+        },
+        home: {
+            h1S1: "Economic, <br> We certify your cards from 8,95 €!",
+            h1S2: "No waiting, <br> Receive them in less than 1 month!",
+            h1S3: "Armored, <br> With the best possible protection!",
+            p01: "WTIG specialists in the authentication and certification of collector's cards",
+            cta: "CERTIFY NOW!",
+            p02: "<strong>Authenticate, protect and grade</strong> your precious card collection with <strong>William & Turner Grading</strong> or either enjoyment or investment - value enhancement and protection assured!",
+            p03: "Search for your cards, select them, check the parameters, fill in the shipping information and you're done!",
+            p04: "At WTIG we are specialists in the authentication and certification of collectible cards, our expert appraisers will inspect, value and score your cards in the fairest and most objective way possible.",
+            h2a: "OUR ADVANTAGES",
+            h2b: "HOW DOES IT WORK?",
+
+        }
+    },
+    esp: {
+        navBar: {
+            tarif: "TARIFAS",
+            services: "SERVICIOS",
+            certify: "¡CERTIFICAR!",
+            myAccount: "MI CUENTA",
+            contact: "CONTACTAR"
+        },
+        footer: {
+            help: "Ayuda",
+            myAccount: "Mi cuenta",
+            services: "Servicios",
+            langage: "Español",
+            aboutUs: "Sobre nostros",
+            tarif: "Tarifas",
+            faqs: "FAQs",
+            contacts: "Contactar",
+            myOrders: "Mis pedidos",
+            warranties: "Nuestras garantias",
+            certifyCard : "Certificar una carta",
+            checkAuth: "Verificar la autenticidad",
+            rightsReserved: "Todos los derechos reservados",
+            legalNotice: "Aviso legal",
+            saleConditions: "Condiciones generales de venta",
+            privacyPolicy: "Politica de privacidad"
+        },
+        home: {
+            h1S1: "Económicos, <br> ¡Certificamos tus cartas a partir de 8,95 €!",
+            h1S2: "Sin esperas, <br> ¡Recíbelas en menos de 1 mes!",
+            h1S3: "Blindada, <br> ¡Con la mejor protección posible!",
+            p01: "WTIG especialistas en la autentificación y la certificación de cartas de colección",
+            cta: "¡CERTIFICAR AHORA!",
+            p02: "<strong>Autentifica, protege y puntúa</strong> tu preciada colección de cartas con <strong>William & Turner Grading</strong> ya sea para disfrute o para inversión. ¡Aumento de valor y protección asegurados!",
+            p03: "Busca tus cartas, selecciónala, comprueba los parámetros, completa los datos de envío y ¡listo!",
+            p04: "En WTIG somos especialistas en la autentificación y la certificación de cartas de colección, nuestros expertos peritos inspeccionaran, valoraran y puntuaran tus cartas de la manera más justa y objetiva posible",
+            h2a: "NUESTRAS VENTAJAS",
+            h2b: "¿CÓMO FUNCIONA?",
+
+        }
+    }
+};
+
+export {langObj};
